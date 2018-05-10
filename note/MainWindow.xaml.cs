4382bc033c0ce5067f8b56db8592ba5caa1f122a
m305-7\w401_ <charlotte0388@gmail.com>
2018-05-10 18:02:59 +0800

    1

---
+++ note/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace note
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.SaveFileDialog dig = new Microsoft.Win32.SaveFileDialog();
+
+            Nullable<bool> result = dig.ShowDialog();
+
+            if(result == true)
+            {
+                System.IO.File.WriteAllText(dig.FileName, Textarea.Text);
+            }
+            
+        }
+
+        private void OpenBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dig = new Microsoft.Win32.OpenFileDialog();
+
+            Nullable<bool> result = dig.ShowDialog();
+
+            if (result == true)
+            {
+                System.IO.File.WriteAllText(dig.FileName, Textarea.Text);
+            }
+
+        }
+    }
+}
