3955ef1077fb4b2a6c1ea5d016dc4c8a535c5a44
DESKTOP-KQBVSJ0\charl <charlotte0388@gmail.com>
2018-06-23 21:05:44 +0800

    3.

---
+++ note/note/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace note
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.SaveFileDialog dig = new Microsoft.Win32.SaveFileDialog();
+
+            Nullable<bool> result = dig.ShowDialog();
+
+            if(result == true)
+            {
+                System.IO.File.WriteAllText(dig.FileName, Textarea.Text);
+            }
+            
+        }
+
+        private void OpenBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dig = new Microsoft.Win32.OpenFileDialog();
+
+            Nullable<bool> result = dig.ShowDialog();
+
+            if (result == true)
+            {
+                System.IO.File.WriteAllText(dig.FileName, Textarea.Text);
+            }
+
+        }
+
+        private void NewBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SaveAsBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Small_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Medium_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Big_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
